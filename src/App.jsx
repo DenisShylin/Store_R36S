631add5e88097e896cae22c51fe3b5b23736cccb
Shylin Denis <tawnship406@gmail.com>
2025-02-16 14:46:49 +0200

    Сохраню перед изменением путей к фото

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "accordion-js/dist/accordion.min.css";
 
 import Header from "./components/Sections/Header/Header";
 import Hero from "./components/Sections/Hero/Hero";
-
 import About from "./components/Sections/About/About";
 import Features from "./components/Sections/Features/Features";
 import Categories from "./components/Sections/Categories/Categories";
@@ -12,24 +13,32 @@ import Reviews from "./components/Sections/Reviews/Reviews";
 import Contact from "./components/Sections/Contact/Contact";
 import Footer from "./components/Sections/Footer/Footer";
 
-import "accordion-js/dist/accordion.min.css";
-
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <Hero />
-        <About />
-        <Features />
-        <Categories />
-        <Articles />
-        <Reviews />
-        <Contact />
-        <Products />
-      </main>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <Hero />
+              <About />
+              <Features />
+              <Categories />
+              <Articles />
+              <Reviews />
+              <Contact />
+              <Products />
+            </main>
+          }
+        />
+        {/* Здесь можно добавить другие маршруты */}
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* <Route path="/products" element={<ProductsPage />} /> */}
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
